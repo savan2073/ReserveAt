afd5218f6a746e99ce11fe1eac4da15ed1013127
bartoszsawaniewski <savan2073@gmail.com>
2023-12-07 23:47:43 +0100

    jwt start

--- src/main/java/com/example/ReserveAt/Service/JwtTokenProvider.java
+++ src/main/java/com/example/ReserveAt/Service/JwtTokenProvider.java
@@ -1,9 +1,15 @@
 package com.example.ReserveAt.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.attribute.UserPrincipal;
+import java.util.Date;
 
 
 @Service
@@ -13,16 +19,35 @@ public class JwtTokenProvider {
 
     //metoda do generowania tokena JWT
     public String generateToken(Authentication authentication) {
-        //logika generowania tokena
+        //uzyskanie nazwy użytkownika z obiektu authentication
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + 864000000);
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getName())
+                .setIssuedAt(new Date())
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS512, "")
+                .compact();
     }
 
     //metoda weryfikacji tokena JWT
     public boolean validateToken(String token) {
-        //logika weryfikacji tokena
+        try {
+            Jwts.parser().setSigningKey("");
+            return true;
+        }
     }
 
     //metoda wydobywania informacji uzytkownika z tokena
     public String getUsernameFromToken(String token) {
-        //logika wydobycia informacji
+        Claims claims = Jwts.parser()
+                .setSigningKey("")
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
     }
 }
